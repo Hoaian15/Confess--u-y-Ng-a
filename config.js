6343e04710882da49ca516aa9748d3b503d90361
Hoaian15 <108233407+Hoaian15@users.noreply.github.com>
2022-06-27 01:02:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Đêm rảnh quá mà",
+    introTitle: 'kong vịt:>',
+    introDesc: `cho mềnh hỏi bạn vài ba câu he',
+    btnIntro: '^^Ỏ^^',
+    title: 'bạn xú tính á hệ, ngựa ghê',
+    desc: 'Mềh biết mà . thông cảm cho bạn',
+    btnYes: 'sao biế́t hay v đr đó',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    question: 'mềnh chả bh bịp bạn nhề',
+    btnReply: 'Gửi cho chụy ',
+    reply: 'ỏ nai sừ. chỉ có mình hay bịp thoi',
+    mess: 'Mình biết mà . wúy bạn nhìu 😘😘',
+    messDesc: 'chúc bạn ngủ giật mình cà tưng cà tưng he',
+    btnAccept: 'Kê bạn nè',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
