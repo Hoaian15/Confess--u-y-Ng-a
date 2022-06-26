ed22476a1e8d2a65baf0291c7d9f9583d478b6aa
Hoaian15 <108233407+Hoaian15@users.noreply.github.com>
2022-06-27 00:42:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     reply: 'đúng ròi chỉ có mình bịp thoy',
     mess: 'Mình biết mà. wúy bạn nhìu 😘😘',
     question: 'Nhớ là nhớ mềnh rố',
-    btnReply: 'Gửi cho chụy nè',
+    btnReply: 'Gửi cho chụy',
     reply: 'Kê luôn nề',
     messLink: 'https://github.com/zukahai/Confess-Đũy Ngựa' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
